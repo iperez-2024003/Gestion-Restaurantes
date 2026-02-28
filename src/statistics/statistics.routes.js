d70f4de17404e52ab515a406e594372cb95d25ab
iperez-2024003 <iperez-2024003@kinal.edu.gt>
2026-02-27 21:04:50 -0600

    feat: validar param id y query period/limit en estadísticas

--- src/statistics/statistics.routes.js
+++ src/statistics/statistics.routes.js
@@ -9,12 +9,13 @@ import {
 } from './statistics.controller.js';
 import { validateJWT } from '../../middlewares/validate-JWT.js';
 import { requireAdmin } from '../../middlewares/require-role.js';
+import { validateUuidParam, validateQueryPeriod, validateQueryLimit } from '../../middlewares/validate-params.js';
 
 const router = Router();
 
-router.get('/restaurant/:id/overview', [validateJWT, requireAdmin], getRestaurantOverview);
-router.get('/restaurant/:id/orders', [validateJWT, requireAdmin], getOrdersStats);
-router.get('/restaurant/:id/popular-dishes', [validateJWT, requireAdmin], getPopularDishes);
+router.get('/restaurant/:id/overview', [validateJWT, requireAdmin, validateUuidParam('id')], getRestaurantOverview);
+router.get('/restaurant/:id/orders', [validateJWT, requireAdmin, validateUuidParam('id'), validateQueryPeriod], getOrdersStats);
+router.get('/restaurant/:id/popular-dishes', [validateJWT, requireAdmin, validateUuidParam('id'), validateQueryLimit], getPopularDishes);
 router.get('/platform/summary', [validateJWT, requireAdmin], getPlatformSummary);
 
 export default router;
