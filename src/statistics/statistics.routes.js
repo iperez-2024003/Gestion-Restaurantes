a78b56977e8b8247dc3474c942cb3fca1837ad95
iperez-2024003 <iperez-2024003@kinal.edu.gt>
2026-02-27 19:45:22 -0600

    feat: exigir ADMIN_ROLE en todas las rutas de estadísticas

--- src/statistics/statistics.routes.js
+++ src/statistics/statistics.routes.js
@@ -8,12 +8,13 @@ import {
   getPlatformSummary,
 } from './statistics.controller.js';
 import { validateJWT } from '../../middlewares/validate-JWT.js';
+import { requireAdmin } from '../../middlewares/require-role.js';
 
 const router = Router();
 
-router.get('/restaurant/:id/overview', validateJWT, getRestaurantOverview);
-router.get('/restaurant/:id/orders', validateJWT, getOrdersStats);
-router.get('/restaurant/:id/popular-dishes', validateJWT, getPopularDishes);
-router.get('/platform/summary', validateJWT, getPlatformSummary);
+router.get('/restaurant/:id/overview', [validateJWT, requireAdmin], getRestaurantOverview);
+router.get('/restaurant/:id/orders', [validateJWT, requireAdmin], getOrdersStats);
+router.get('/restaurant/:id/popular-dishes', [validateJWT, requireAdmin], getPopularDishes);
+router.get('/platform/summary', [validateJWT, requireAdmin], getPlatformSummary);
 
 export default router;
