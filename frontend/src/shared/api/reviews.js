cb03aae442d1e36db38798ea3117163f7c2a8c79
jmendez-2021550 <jmendez-2021550@kinal.edu.gt>
2026-05-01 12:31:50 -0600

    feat: servicio para el manejo de comentarios y calificaciones de clientes

---
+++ frontend/src/shared/api/reviews.js
@@ -0,0 +1,5 @@
+import api from './axios';
+
+export const createReview = (data) => api.post('/reviews', data);
+export const getRestaurantReviews = (restaurantId) => api.get(`/reviews/restaurant/${restaurantId}`);
+
